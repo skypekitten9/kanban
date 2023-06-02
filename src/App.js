1ca07d8fa68231b21bd1bc0bc20b451b82f01035
Victor Enhörning <victoren@axis.com>
2023-06-02 14:46:37 +0200

    Board layout, first draft

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display:'grid', 
+      gridTemplateAreas: "to-do in-progress test done",
+      gridTemplateColumns: "1fr 1fr 1fr 1fr",
+      gridTemplateRows: "100vh"
+      }}>
+      <div style={{gridArea: 'to-do'}}></div>
+      <div style={{gridArea: 'in-progress'}}></div>
+      <div style={{gridArea: 'test'}}></div>
+      <div style={{gridArea: 'done'}}></div>
     </div>
   );
 }
