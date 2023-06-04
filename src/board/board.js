7f56290696b41eb5b1122eb642574bfc057c15b3
Victor Enhörning <victoren@axis.com>
2023-06-04 10:01:19 +0200

    Added first draft cards.

--- src/board/board.js
+++ src/board/board.js
@@ -2,7 +2,11 @@ import './board.css'
 export default function Board() {
     return (
         <div className="board">
-            <div className="boardColumn" id='to-do'></div>
+            <div className="boardColumn" id='to-do'>
+                <div className='card'></div>
+                <div className='card'></div>
+                <div className='card'></div>
+            </div>
             <div className="boardColumn" id='in-progress'></div>
             <div className="boardColumn" id='test'></div>
             <div className="boardColumn" id='done'></div>
