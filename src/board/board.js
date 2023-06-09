fb726ff6fd131a1f0d96843fc44ce8baee82d77b
Victor Enhörning <victoren@axis.com>
2023-06-09 14:09:09 +0200

    Simple static cards can now be added via button

--- src/board/board.js
+++ src/board/board.js
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import './board.css'
 import './card.js'
 import Card from './card.js';
 export default function Board() {
+    const [list, setList] = useState([])
+
+    const AddCard = () => {
+        console.log("Adding card...");
+        setList([...list, <Card></Card>]);
+
+    };
+
     return (
         <div className="board">
             <div className="boardColumn" id='to-do'>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                {list.map((item) =>  {return item;})}
             </div>
             <div className="boardColumn" id='in-progress'></div>
             <div className="boardColumn" id='test'></div>
-            <div className="boardColumn" id='done'></div>
+            <div className="boardColumn" id='done'>
+                <button onClick={() => AddCard()}>Add card</button>
+            </div>
         </div>
     );
 }
